33aff8edc1d508961350ee6f87f694d269f5e34d
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-12-14 16:53:44 +0100

    Completely built anh styled the feeds component

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./Post.css";
-import { Avatar } from "@material-ui/core"
-
+import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import
+ExpandMoreIcon
+from '@material-ui/icons/ExpandMore';
 
 function Post({ profilePic, image, userName, timeStamp, message }) {
     return ( <
@@ -43,6 +49,45 @@ function Post({ profilePic, image, userName, timeStamp, message }) {
         <
         /
         div >
+
+        <
+        div className = "post_options" >
+        <
+        div className = "post_option" >
+        <
+        ThumbUpIcon / >
+        <
+        p > Like < /p>
+
+
+        <
+        /div>
+
+
+        <
+        div className = "post_option" > < ChatBubbleOutlineIcon / >
+        <
+        p > Comment < /p>
+
+        <
+        /div > <
+        div className = "post_option" > < NearMeIcon / >
+        <
+        p > Share < /p> < /
+        div > <
+        div className = "post_option" > < AccountCircleIcon / >
+        <
+        ExpandMoreIcon / >
+
+
+        <
+        /
+        div >
+
+
+
+        <
+        /div>
 
         <
         /div>
